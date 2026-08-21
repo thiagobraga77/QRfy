563293000c0154b0c78c74e78fd906b3e085bfd6
Thiago Braga <thiagobraga2a@gmail.com>
2026-08-21 19:21:31 -0300

    feat: adds the functionality for the QR Code generation process

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,43 @@
-console.log("hello");
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
 
+const qrCodeInput = document.querySelector("#qr-form input");
+
+const qrCodeImg = document.querySelector("#qr-code img")
+
+
+// Events
+
+// Generate QR Code
+function generateQrCode() {
+    const qrCodeInputValue = qrCodeInput.value;
+
+    if (!qrCodeInputValue) return;
+
+    qrCodeBtn.innerText = "Generating code...";
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+    qrCodeImg.addEventListener("load", () => {
+        container.classList.add("active");
+        qrCodeBtn.innerText = "Code created";
+
+    })
+}
+
+qrCodeBtn.addEventListener("click", () => {
+    generateQrCode();
+});
+
+qrCodeInput.addEventListener("keydown", (e) => {
+    if (e.code === "Enter") {
+        generateQrCode();
+    }
+});
+
+// Clean the QR Code area
+qrCodeInput.addEventListener("keyup", () => {
+    if (!qrCodeInput.value) {
+        container.classList.remove("active");
+        qrCodeBtn.innerText = "Generate QR Code";
+    }
+})
